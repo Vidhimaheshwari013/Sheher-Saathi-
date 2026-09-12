03a07feb92b6ad97c02b20019b125e6e660c1268
ananya-jais <ananyajaiswal070@gmail.com>
2026-09-12 22:08:19 +0530

    Integration and updation

--- backend/schemas.py
+++ backend/schemas.py
@@ -4,6 +4,9 @@ from datetime import datetime
 
 class ComplaintCreate(BaseModel):
     raw_text: str
+    location: str | None = None
+    latitude: float | None = None
+    longitude: float | None = None
 
 class ComplaintOut(BaseModel):
     id: int
